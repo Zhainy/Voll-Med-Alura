b975a96d98cc6d74f24942c6f356eb6a6f40edd8
Zhainy <ni.ferng@gmail.com>
2026-03-06 18:28:18 -0300

    Se utiliza JUnit, Mockito para Testing

--- api/src/test/java/med/voll/api/controller/ConsultaControllerTest.java
+++ api/src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -1,32 +1,76 @@
 package med.voll.api.controller;
 
+import med.voll.api.domain.consulta.DatosDetalleConsulta;
+import med.voll.api.domain.consulta.DatosReservaConsulta;
+import med.voll.api.domain.consulta.ReservaDeConsultas;
+import med.voll.api.dto.Especialidad;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class ConsultaControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
     @Autowired
-    private ConsultaController consultaController;
+    private JacksonTester<DatosReservaConsulta> datosReservaConsultaJson;
+
+    @Autowired
+    private JacksonTester<DatosDetalleConsulta> datosDetalleConsultaJson;
+
+    @MockitoBean
+    private ReservaDeConsultas reservaDeConsultas;
 
     @Test
     @DisplayName("Debería devolver HTTP 400 cuando no se envían los datos necesarios para reservar una consulta")
     @WithMockUser
     void reservar_escenario1() throws Exception {
         var response = mockMvc.perform(post("/consultas"))
+                .andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+    @Test
+    @DisplayName("Debería devolver HTTP 200 cuando la request reciba un json válido")
+    @WithMockUser
+    void reservar_escenario2() throws Exception {
+        var fecha = LocalDateTime.now().plusHours(1);
+        var especialidad = Especialidad.CARDIOLOGIA;
+        var datosDetalle = new DatosDetalleConsulta(null, 1L, 5L, fecha);
+        when(reservaDeConsultas.reservar(any())).thenReturn(datosDetalle);
+
+        var response = mockMvc.perform(post("/consultas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(datosReservaConsultaJson.write(
+                            new DatosReservaConsulta(1L, 5L, fecha, especialidad)
+                        ).getJson()
+                    )
+                )
                 .andReturn()
                 .getResponse();
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        var jsonEsperado = datosDetalleConsultaJson.write(
+                datosDetalle
+        ).getJson();
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
 }
